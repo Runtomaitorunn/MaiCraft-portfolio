2eaa9868d1ed988d456ba01cf0dfd64f5408cfe6
Mai <yeahmy1022@gmail.com>
2025-04-26 18:04:15 +0100

    organize bit

---
+++ maicraft/src/app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import homeSrc from "/public/home.jpg";
+import {Metadata} from "next";
+import Hero from "@/components/hero"
+
+export const metadata: Metadata = {
+  title: 'Home',
+}
+
+export default function Page(){
+  return(
+    <Hero imgUrl={homeSrc} altTxt="Home" content = "MaiCraft"/>
+  );
+}
